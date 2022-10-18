701518e6f4144ce548a809f55bc000336b8ff3bd
Jay Jernigan <jjernigan1065@gmail.com>
2022-10-18 14:55:36 -0400

    created featured posts and hero page. added css and updated home page with created components

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+import { Fragment } from 'react'
+import FeaturedPosts from '../components/featured-posts'
+import Hero from '../components/hero'
+
 export default function Home() {
-  return (
-    <div>index</div>
-  )
+	return (
+		<Fragment>
+			<Hero />
+			<FeaturedPosts />
+		</Fragment>
+	)
 }
