635933ebe6c4b761a0d44585878350a0f17ed8cc
Jay Jernigan <jjernigan1065@gmail.com>
2022-10-19 14:42:54 -0400

    sketched out post-item without data

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -1,3 +1,37 @@
-export default function PostItem() {
-	return <div>PostItem</div>
+import Image from '../../node_modules/next/image'
+import Link from '../../node_modules/next/link'
+
+import classes from './post-item.module.css'
+
+export default function PostItem(props) {
+	const { title, image, excerpt, date, slug } = props.post
+
+	const formattedDate = new Date(date).toLocaleDateString('en-US', {
+		day: 'numeric',
+		month: 'long',
+		year: 'numeric',
+	})
+
+	const imagePath = `/images/posts/${slug}/${image}`
+	return (
+		<li className={classes.image}>
+			<Link>
+				<a>
+					<div>
+						<Image
+							src={imagePath}
+							alt={title}
+							width={300}
+							height={200}
+						/>
+					</div>
+					<div>
+						<h3>{title}</h3>
+						<time>{formattedDate}</time>
+						<p>{excerpt}</p>
+					</div>
+				</a>
+			</Link>
+		</li>
+	)
 }
--- components/posts/posts-grid.js
+++ components/posts/posts-grid.js
@@ -1,3 +1,15 @@
-export default function PostsGrid() {
-	return <div>PostsGrid</div>
+import PostItem from './post-item'
+
+import classes from './posts-grid.module.css'
+
+export default function PostsGrid(props) {
+	const { posts } = props
+
+	return (
+		<ul className={classes.grid}>
+			{posts.map(post => (
+				<PostItem />
+			))}
+		</ul>
+	)
 }
