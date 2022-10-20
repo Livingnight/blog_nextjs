1d8e4c4cfff756d17bde251639d552e9ab8d2069
Jay Jernigan <jjernigan1065@gmail.com>
2022-10-20 12:28:01 -0400

    retrieving and rendering data for a single post

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,5 +1,32 @@
 import PostContent from '../../components/post_detail/post-content'
 
-export default function PostDetails() {
-	return <PostContent />
+import { getPostData, getPostsFiles } from '../../lib/posts-util'
+
+export default function PostDetails(props) {
+	return <PostContent post={props.post} />
+}
+
+export function getStaticProps(context) {
+	const { params } = context
+	const { slug } = params
+
+	const postData = getPostData(slug)
+
+	return {
+		props: {
+			post: postData,
+		},
+		revalidate: 600,
+	}
+}
+
+export function getStaticPaths() {
+	const postFilenames = getPostsFiles()
+
+	const slugs = postFilenames.map(filename => filename.replace(/\.md$/, ''))
+
+	return {
+		paths: slugs.map(slug => ({ params: { slug: slug } })),
+		fallback: false,
+	}
 }
